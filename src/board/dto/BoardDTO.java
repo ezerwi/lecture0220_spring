0a6d55988189a0205e82b6ee766e782d639c062b
johnwi <johnwi@knou.ac.kr>
2020-02-20 15:34:10 +0900

    BoardCommand.java, BoardDTO.java 완성

--- src/board/dto/BoardDTO.java
+++ src/board/dto/BoardDTO.java
@@ -1,9 +1,65 @@
 /*
  * jsp / Controller / DAO 간 데이터 전달 역할
+ * DTO 부분 table name = springboard
  */
 
 package board.dto;
 
 public class BoardDTO {
+
+	public BoardDTO() {
+
+	}
+
+	private int num, readcnt;
+	private String author, title, content, date;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getReadcnt() {
+		return readcnt;
+	}
+
+	public void setReadcnt(int readcnt) {
+		this.readcnt = readcnt;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
 
 }
