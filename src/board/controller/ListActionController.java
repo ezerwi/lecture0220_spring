fcc1f608cadce9da31c5c50537bb1997bfe8e1c8
johnwi <johnwi@knou.ac.kr>
2020-02-20 16:12:11 +0900

    ListActionController.java

--- src/board/controller/ListActionController.java
+++ src/board/controller/ListActionController.java
@@ -7,18 +7,48 @@
 
 package board.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class ListActionController implements Controller {
+import board.dao.BoardDAO;
+import board.dto.BoardDTO;
 
+// Controller 상속받는 이유 : 요청 받아서 처리하기 위함
+public class ListActionController implements Controller {
+	
+	
+	private BoardDAO dao;
+
+	public void setDao(BoardDAO dao) {
+		this.dao = dao;
+		System.out.println("SUCCESS_call_setDao()__"+dao);
+	}
+
+	/*
+	 * dao 는 board-servlet.xml <bean name = "list.do"~~> <property ~~~>에 의해 주입
+	 *
+	 */
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		
+		System.out.println("__CALL__ListActionController___");
+		
+		ArrayList<BoardDTO> list = dao.list();
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("list");
+		// list.jsp
+		// ViewResolver에서 .jsp 추가했기 때문에 안써도 됨
+		
+		mav.addObject("list", list);
+		// servlet에서 request.setAttribute()와 동일한 역할
+		
+		return mav;
+		// DispatcherServlet에 전달
 	}
 
 }
